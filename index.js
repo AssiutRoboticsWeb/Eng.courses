dae3b4a8301eb1257b699e0c1ec6f046adc1f287
mazen magdy <69149378+Mazen-magdy@users.noreply.github.com>
2025-07-30 04:46:53 +0300

    adding node modules

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000; 
-
+  
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,11 +32,11 @@ app.use((req, res, next) => {
 
 const authRoutes = require("./routes/auth");
 const trackRoutes = require("./routes/track");
-
-
+ 
+  
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-
+  
 app.use("/api/auth", authRoutes); 
 app.use("/api/track", trackRoutes);
 
---
+++ routes/track.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const router=express.Router()
+const TrackController=require('../controller/track.controler')
+const checkRole=require('../middlewares/checkRole')
+// crud tracks
+const auth = require("../middlewares/jwt").auth
+
+
+router.get('/',TrackController.getAllTracks);
+
+router.use(auth,checkRole("manager","admin"))
+
+router.post('/',TrackController.addTrack);
+
+
+
+
+
+router.route('/:id')
+.get(TrackController.getTrackById)
+.put(TrackController.editTrack)
+.delete(TrackController.deleteTrack);
+
+
+
+
+
+
+module.exports=router
