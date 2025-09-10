9ea87f114b32f21217ea02bb324ddbe254583625
karenmedhat12 <Karenmedhat784@gmail.com>
2025-09-10 15:07:59 +0300

    track schema

--- models/track.js
+++ models/track.js
@@ -1,47 +1,39 @@
-const monggose=require('mongoose')
+const mongoose = require('mongoose')
 
-const YearSchema=monggose.Schema({
-    name:{
-        type:String,
-        required:[true,"Name is required"]
+const YearSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
     },
-    firstSemester:{
-        subjects:[{
-            type:monggose.Schema.Types.ObjectId,
-            ref:"Subject"
-        }]  
-    },
-    secondSemester:{
-        subjects:[{
-            type:monggose.Schema.Types.ObjectId,
-            ref:"Subject"
+    firstSemester: {
+        subjects: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Subject"
         }]
     },
-
-})
-
-
-
-const TrackSchema=monggose.Schema({
-    name:{
-        type:String,
-        required:[true,"Name is required"]
-    },
-    // add 5 years by default
-    years:[
-        YearSchema
-    ],
-    type:{
-        type:String,
-        enum:["general","credit"],
-        required:[true,"Type is required"]
+    secondSemester: {
+        subjects: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Subject"
+        }]
     }
 })
 
+const TrackSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    years: [YearSchema], 
+    type: {
+        type: String,
+        enum: ["general", "credit"],
+        required: [true, "Type is required"]
+    }
+})
 
-
-
-module.exports={
-    Track:monggose.model('Track',TrackSchema),
-    YearSchema:YearSchema
+module.exports = {
+    Track: mongoose.model('Track', TrackSchema),
+    years: mongoose.model('year',YearSchema ),
 }
+
